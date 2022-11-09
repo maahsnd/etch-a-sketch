deda715e8f7e57d66192dae2955355351e74e744
Replit user <>
2022-11-09 21:13:56 +0000

    Create 16 by 16 grid
    using CSS grid for educational purposes

---
+++ style.css
@@ -0,0 +1,12 @@
+const gridContainer = document.querySelector('.gridContainer');
+
+for (j = 0; j < 16; j++){
+    for (i = 0; i < 16; i++) {
+        const div = document.createElement('div');
+        div.setAttribute('id', 'theDiv'+i);
+        div.setAttribute('class', 'gridDiv');
+        div.style.cssText = 'border: 1px black solid; aspect-ratio: 1;';
+        gridContainer.appendChild(div);
+        console.log(8);
+    }
+}

f10121c7bd569a067c594bb2f6293b00f210a6aa
Replit user <>
2022-11-10 21:51:27 +0000

    Add reset grid feature Bug: grid spans more than screen height, making it visually unappealing. Bug2: when grid is updated, cells change to reflext new size, but retain color from previous grid. Defeats purpose of changing grid.

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
 const gridContainer = document.querySelector('.gridContainer');
+let gridSize;
 
-for (i = 0; i < (16 * 16); i++) {
-    const div = document.createElement('div');
-    div.setAttribute('id', 'theDiv'+i);
-    div.setAttribute('class', 'gridDiv');
-    console.log(div.getAttribute('id'));
-    div.style.cssText = 'border: 1px black solid; aspect-ratio: 1;';
-    div.addEventListener('mouseover', (e) => {
-        //console.log(e);
-        e.target.style.backgroundColor = 'purple';
-        e.stopPropagation();
-    });
-    gridContainer.appendChild(div);
+function updateGrid(){
+    do {
+        gridSize = prompt("State grid side length", 16);
+    } while (gridSize > 100 && gridSize < 5); 
+    return(setGridTemplate(gridSize))
 }
+
+function setGridTemplate(gridSize = 16) {
+
+    let templateString= '';
+
+    for (i = 0; i < gridSize; i++){
+        templateString += '1fr ';
+    }
+
+    templateString = "grid-template-columns: " + templateString + ";";
+    gridContainer.style.cssText = templateString;
+
+    makeGrid(gridSize);
+
+    function makeGrid(gridSize = 16){
+
+        for (i = 0; i < (gridSize * gridSize); i++) {
+            const div = document.createElement('div');
+            div.setAttribute('id', 'theDiv'+i);
+            div.setAttribute('class', 'gridDiv');
+            div.style.cssText = 'border: 1px black solid; aspect-ratio: 1;';
+            div.addEventListener('mouseover', (e) => {
+                e.target.style.backgroundColor = 'purple';
+                e.stopPropagation();
+            });
+            gridContainer.appendChild(div);
+        }
+    }
+}
+
+setGridTemplate(gridSize);
